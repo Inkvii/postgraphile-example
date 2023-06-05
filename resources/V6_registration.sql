c56a718e74993c6f4e759895eb6fb980c53359fe
Lubos Verzich <lve@paymorrow.com>
2023-06-05 09:51:20 +0200

    Fix - env port 5000 is used by mac air play receiver, so changed it to 5001 Fix - admin user is now set with is_admin=true Refactor - changed volume name of postgres container so it wont collide with other container names that often Feature - added admin only view for stores

--- resources/V6_registration.sql
+++ resources/V6_registration.sql
@@ -3,5 +3,8 @@ begin;
 select registration.register('admin', 'asd123ASD!');
 select registration.register('guest', 'asd123ASD!');
 
+update security.person_account
+set is_admin = true
+where username = 'admin';
 
 commit;
