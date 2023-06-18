049fd8f2d8a28693bd60b83ab112297f7919d928
Inkvii <22601194+inkvii@users.noreply.github.com>
2023-06-18 20:07:06 +0200

    Added manual token verification

--- src/index.mjs
+++ src/index.mjs
@@ -2,8 +2,10 @@ import { postgraphile } from "postgraphile"
 import dotenv from "dotenv"
 import { postgraphileOptions } from "./postgraphileOptions.mjs"
 import express from "express"
+import * as crypto from "crypto"
 
 dotenv.config()
+
 
 const middleware = postgraphile(process.env.DATABASE_URL, ["public", "registration"], postgraphileOptions)
 
@@ -15,6 +17,19 @@ function authorizationCheck(req, res, next) {
   next()
 }
 
+app.use("/verifyToken", (req, res, next) => {
+  console.log("verify token", req.body)
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYXV0aGVudGljYXRlZCIsImV4cCI6MTY4NzExNDYyNSwidXNlcl9pZCI6MSwiaXNfYWRtaW4iOmZhbHNlLCJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjg3MTA4NjI0LCJhdWQiOiJwb3N0Z3JhcGhpbGUiLCJpc3MiOiJwb3N0Z3JhcGhpbGUifQ.tZ0OtsTYtrHzsQLbhY8YNBnZnc087UsIZ9p7s4MTEb8"
+  const [header, payload, signature] = token.split(".")
+  const [decodedHeader, decodedPayload] = token.split(".").map((part, index, array) => index < array.length - 1 ? Buffer.from(part, "base64url").toString() : part)
+
+  const verifier = crypto.createHmac("sha-256", "my-super-secret").update([header, payload].join(".")).digest().toString("base64url")
+  console.log("Parsed jwt", decodedHeader, decodedPayload)
+  console.log("Signature from token", signature)
+  console.log("Signature from verifier", verifier.toString())
+  console.info("Does signature match?", signature === verifier.toString())
+
+})
 app.use("/graphql", authorizationCheck)
 app.use(middleware)
 
